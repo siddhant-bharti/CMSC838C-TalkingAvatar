bcc06e2ae557897cca4528ca48a41e690cc46d74
siddhant-bharti <sbharti51@gmail.com>
2024-03-18 15:05:19 -0400

    Adds test_chat.py

--- ChatServer/test_chat.py
+++ ChatServer/test_chat.py
@@ -1,25 +1,37 @@
-from transformers import pipeline
-
-# Initialize the pipeline for text-generation using the Llama 2 model
-generator = pipeline('text-generation', model='meta-llama/Llama-2-7b-chat-hf')
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+from threading import Thread
 
 
 # Function to simulate chat with Oprah Winfrey
 def chat_with_oprah(user_input):
+    generator = pipeline('text-generation', model='meta-llama/Llama-2-7b-chat-hf')
     # Crafting a prompt that includes Oprah's persona
     prompt = f"User: {user_input}\nOprah Winfrey: "
 
     # Generating Oprah Winfrey's response
-    responses = generator(prompt, max_length=150, num_return_sequences=1)
+    responses = generator(prompt, max_new_tokens=3, num_return_sequences=1)
 
     # Extracting and printing the generated text
     oprah_response = responses[0]['generated_text'].split("Oprah Winfrey: ")[-1]
-    return oprah_response
+    print(oprah_response)
 
 
-# Example user input
-user_input = "What's your secret to success?"
+def chat_with_oprah_streaming(user_input):
+    tok = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+    inputs = tok([user_input], return_tensors="pt")
+    streamer = TextIteratorStreamer(tok)
+    generation_kwargs = dict(inputs, streamer=streamer, max_new_tokens=5)
+    thread = Thread(target=model.generate, kwargs=generation_kwargs)
+    thread.start()
+    generated_text = ""
+    for new_text in streamer:
+        print(new_text)
+        generated_text += new_text
+    print(generated_text)
 
-# Get Oprah's response
-oprah_response = chat_with_oprah(user_input)
-print(f"Oprah Winfrey: {oprah_response}")
+
+if __name__ == '__main__':
+    user_input = "What's your secret to success?"
+    # chat_with_oprah(user_input)
+    chat_with_oprah_streaming(user_input)
