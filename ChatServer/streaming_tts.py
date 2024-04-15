3e0214bd45f5f600c5186649d1dfbb3e86a18fa0
siddhant-bharti <sbharti51@gmail.com>
2024-04-15 10:57:06 -0400

    Clean up tts code

--- ChatServer/streaming_tts.py
+++ ChatServer/streaming_tts.py
@@ -5,19 +5,17 @@ import torchaudio
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
 
-print("Loading model...")
+
 config = XttsConfig()
 config.load_json("/home/lyhan12/Workspace/siddhantbharti/CMSC838C/CMSC838C-TalkingAvatar/models/tts/tts_models--multilingual--multi-dataset--xtts_v2/config.json")
 model = Xtts.init_from_config(config)
 model.load_checkpoint(config, checkpoint_dir="/home/lyhan12/Workspace/siddhantbharti/CMSC838C/CMSC838C-TalkingAvatar/models/tts/tts_models--multilingual--multi-dataset--xtts_v2")
 model.cuda()
 
-print("Computing speaker latents...")
 gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=["oprah1.wav"])
 
-print("Inference...")
 
-def run_tts_streaming(text_to_speak, file_to_save):
+def run_tts_streaming(text_to_speak, file_to_save=None):
     t0 = time.perf_counter()
     chunks = model.inference_stream(
         text_to_speak,
@@ -32,8 +30,16 @@ def run_tts_streaming(text_to_speak, file_to_save):
             print(f"Time to first chunck: {time.perf_counter() - t0}")
         print(f"Received chunk {i} of audio length {chunk.shape[-1]}")
         wav_chuncks.append(chunk)
-        # torchaudio.save(f"stream_{i}.wav", chunk.squeeze().unsqueeze(0).cpu(), 24000)
     wav = torch.cat(wav_chuncks, dim=0)
-    torchaudio.save(file_to_save, wav.squeeze().unsqueeze(0).cpu(), 24000)
     t1 = time.perf_counter()
-    print(f"Creating audio took {t1-t0} secs")
+    print(f"Creating audio took {t1 - t0} secs")
+    if file_to_save:
+        torchaudio.save(file_to_save, wav.squeeze().unsqueeze(0).cpu(), 24000)
+    else:
+        return wav
+
+
+if __name__ == '__main__':
+    text_to_speak = ask_me_something = input(">>Generate streaming audio for: ")
+    file_to_save = "./tts_testing_streaming.wav"
+    run_tts_streaming(text_to_speak, file_to_save)
--- ChatServer/tts.py
+++ ChatServer/tts.py
@@ -1,26 +1,25 @@
 import time
-import torch
+
 from TTS.api import TTS
 
-# Get device
-device = "cuda" if torch.cuda.is_available() else "cpu"
+from utils import get_device
 
+# Get device
+device = get_device()
 
 # Init TTS
+# Model with best audio quality I could find is tts_models/multilingual/multi-dataset/xtts_v2
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 
 def run_tts(text_to_speak, file_to_save):
-    tts.tts_to_file(text=text_to_speak, speaker_wav="oprah1.wav", language="en", file_path=file_to_save)
+    t1 = time.perf_counter()
+    tts.tts_to_file(text=text_to_speak, speaker_wav="./oprah1.wav", language="en", file_path=file_to_save)
+    t2 = time.perf_counter()
+    print(f"Creating audio took {t2 - t1} secs")
 
 
 if __name__ == '__main__':
-    # Run TTS
-    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-    # Text to speech list of amplitude values as output
-
-    t1 = time.perf_counter()
-    text_to_speak = "Oh no, I can tell you're feeling stressed!  It's completely normal to feel overwhelmed at times, but there are many things you can do to manage stress. Have you tried taking a few deep breaths, going for a walk, or practicing some relaxation techniques like meditation or yoga? These can really help calm your mind and body. 🧘‍♀️ If you ever need to talk about it, I'm here to listen. 🤗"
-    wav = tts.tts_to_file(text=text_to_speak, speaker_wav="oprah1.wav", language="en", file_path="../../input5.wav")
-    t2 = time.perf_counter()
-    print(f"Creating audio took {t2-t1} secs")
+    text_to_speak = ask_me_something = input(">>Generate audio for: ")
+    file_to_save = "./tts_testing_vanilla.wav"
+    run_tts(text_to_speak, file_to_save)
